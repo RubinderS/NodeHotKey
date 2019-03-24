653ebf4652c9f8950b56564c4601986368fb8f2e
Rubinder <mail2rubinder@gmail.com>
2019-03-24 22:21:10 +1100

    exposed matchCurrentWindowTitle() function to nodehotkey import

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './NodeHotKey/NodeHotKey';
 export * from './NodeHotKey/utils/KeyboardMouse';
 export * from './NodeHotKey/utils/Keycodes';
 export * from './NodeHotKey/utils/Clipboard';
+export * from './NodeHotKey/utils/Window';
